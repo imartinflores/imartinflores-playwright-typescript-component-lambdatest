bbbc984db9c10de8af34201f8308ad8b35b08aaa
Ignacio Martin Flores <imartinflores@gmail.com>
2022-09-08 17:51:05 +0100

    Initial

---
+++ src/header.js
@@ -0,0 +1,10 @@
+
+function Header () {
+  return (
+    <header>
+       <h1>Header Testing Component</h1>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/tests/component.spec.tsx
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/experimental-ct-react';
+import Header from '../../src/header';
+import App from '../../src/App';
+
+test.use({ viewport: { width: 500, height: 500 } });
+
+test('Header should work', async ({ mount, page }) => {
+  const component = await mount(<Header></Header>);
+  await expect(page.locator("header>h1")).toHaveScreenshot()
+  page.close()
+});
+
+test('App should work', async ({ mount }) => {
+  const component = await mount(<App></App>);
+  await expect(component).toContainText('Learn React');
+});
